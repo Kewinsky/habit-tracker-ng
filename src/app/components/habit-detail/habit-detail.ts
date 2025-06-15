9cac5f7aba433447dc3751394f836eea6b16a1ea
“KewinTaoAnh” <kewin.taoanh@kainos.com>
2025-06-15 18:38:04 +0200

    add necessary components

---
+++ src/app/components/habit-detail/habit-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-detail',
+  imports: [],
+  templateUrl: './habit-detail.html',
+  styleUrl: './habit-detail.scss'
+})
+export class HabitDetail {
+
+}
