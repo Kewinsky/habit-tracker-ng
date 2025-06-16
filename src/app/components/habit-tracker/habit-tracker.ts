90ac3c58dbbab02872534cec915b84aee5518646
“KewinTaoAnh” <kewin.taoanh@kainos.com>
2025-06-16 14:17:54 +0200

    remove habit tracker view

--- src/app/components/habit-tracker/habit-tracker.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-habit-tracker',
-  imports: [],
-  templateUrl: './habit-tracker.html',
-  styleUrl: './habit-tracker.scss',
-})
-export class HabitTracker {}
