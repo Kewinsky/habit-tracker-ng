8e87c8cd3f7548b0c17d54405e88ed5b723a02bd
“KewinTaoAnh” <kewin.taoanh@kainos.com>
2025-06-16 14:18:31 +0200

    align routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { HabitList } from './components/habit-list/habit-list';
-import { HabitDetail } from './components/habit-detail/habit-detail';
-import { HabitForm } from './components/habit-form/habit-form';
+import { HabitListComponent } from './components/habit-list/habit-list';
+import { HabitDetailComponent } from './components/habit-detail/habit-detail';
+import { HabitFormComponent } from './components/habit-form/habit-form';
 
 export const routes: Routes = [
-  { path: '', component: HabitList },
-  { path: 'habit/:id', component: HabitDetail },
-  { path: 'habit/:id/edit', component: HabitForm },
-  { path: 'add', component: HabitForm },
+  { path: '', redirectTo: '/habits', pathMatch: 'full' },
+  { path: 'habits', component: HabitListComponent },
+  { path: 'habits/new', component: HabitFormComponent },
+  { path: 'habits/edit/:id', component: HabitFormComponent },
+  { path: 'habits/:id', component: HabitDetailComponent },
+  { path: '**', redirectTo: '/habits' },
 ];
