1765e8dfd2beabd4f1102cc8d00b4b5675c4a385
“KewinTaoAnh” <kewin.taoanh@kainos.com>
2025-06-18 19:51:32 +0200

    env credentials

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "your-api-key",
+    authDomain: "your-app.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-sender-id",
+    appId: "your-app-id",
+    measurementId: "your-measurement-id",
+  },
+};
